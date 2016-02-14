eba8c562936eb8c45e5e2ffccb3829f8c0cf430e
Julian Nicholls <juliannicholls29@gmail.com>
2016-02-14 19:14:52 +0000

    Change to bitwise storage, allowing for 42x21.
    The alive-dead status is now stored as single bits.
    
    Correct neighbours - it was counting the current cell's status, which is
    fine for empty cells, but wrong for live cells.
    
    Add a display of the generation number in the bottom-right corner.
    
    Three generations are made a second, up from two.

--- conway-life-128x64/conway-life-128x64.ino
+++ conway-life-128x64/conway-life-128x64.ino
@@ -2,15 +2,17 @@
 
 U8GLIB_ST7920_128X64_1X u8g(10);    // Hardware SPI, 10 = CS/CE, 11 = MOSI, 13 = SCK
 
-const int rows  = 64 / 3;
-const int cols  = 128 / 4;
+const int cols  = 128 / 3;  // 42
+const int rows  = 64 / 3;   // 21
+
+const int bytes = (cols >> 3) + 1;
+
 const int top   = 0;
 const int left  = 0;
 
-const int DEAD  = false;
-const int ALIVE = true;
- 
-boolean grid[rows][cols];
+byte  grid[rows][bytes];
+byte  next[rows][bytes];
+int   generation = 0;
 
 struct _live {
   int y;
@@ -18,39 +20,27 @@ struct _live {
 };
 
 struct _live alive_cells[] = {
-  {1, 6}, {1, 7}, {1, 8}, {1, 20}, {1, 21}, {1, 22},
-  {2, 5}, {2, 9}, {2, 19}, {2, 23},
-  {3, 4}, {3, 5}, {3, 10}, {3, 18}, {3, 23}, {3, 24},
-  {4, 3}, {4, 5}, {4, 7}, {4, 8}, {4, 10}, {4, 11}, {4, 17}, {4, 18}, {4, 20}, {4, 21}, {4, 23}, {4, 25},
-  {5, 2}, {5, 3}, {5, 5}, {5, 10}, {5, 12}, {5, 13}, {5, 15}, {5, 16}, {5, 18}, {5, 23}, {5, 25}, {5, 26},
-  {6, 1}, {6, 6}, {6, 10}, {6, 13}, {6, 15}, {6, 18}, {6, 22}, {6, 27},
-  {7, 13}, {7, 15},
-  {8, 1}, {8, 2}, {8, 10}, {8, 11}, {8, 13}, {8, 15}, {8, 17}, {8, 18}, {8, 26}, {8, 27},
-  {9, 13}, {9, 15},
-  {10, 7}, {10, 8}, {10, 9}, {10, 19}, {10, 20}, {10, 21},
-  {11, 7}, {11, 11}, {11, 21},
-  {12, 7}, {12, 9}, {12, 14}, {12, 15}, {12, 16},
-  {13, 13}, {13, 16}, {13, 21}, {13, 22},
-  {14, 16},
-  {15, 12}, {15, 16},
-  {16, 12}, {16, 16},
-  {17, 16},
-  {18, 13}, {18, 15},
+  {1, 4}, {1, 5}, {1, 6},
+  {2, 4}, {2, 7}, {2, 8}, {2, 9},
+  {3, 5}, {3, 7},
+  {4, 1}, {4, 2}, {4, 10},
+  {5, 1}, {5, 3}, {5, 8}, {5, 11},
+  {6, 1}, {6, 10}, {6, 11},
+  {7, 2}, {7, 3},
+  {8, 2}, {8, 5}, {8, 11}, {8, 13}, {8, 14},
+  {9, 2}, {9, 12}, {9, 13}, {9, 15},
+  {10, 4}, {10, 6}, {10, 13}, {10, 14}, {10, 17},
+  {11, 5}, {11, 6}, {11, 8}, {11, 13}, {11, 14}, {11, 18},
+  {12, 9}, {12, 17},
+  {13, 8}, {13, 9}, {13, 10}, {13, 11}, {13, 15}, {13, 17},
+  {14, 8}, {14, 10}, {14, 11}, {14, 15}, {14, 16}, {14, 17}, {14, 18},
+  {15, 9}, {15, 13}, {15, 14}, {15, 16},
+  {16, 14}, {16, 15},
+  {17, 10}, {17, 12}, {17, 13}, {17, 14},
+  {18, 11}, {18, 14},
 };
 
 const int num_alive_cells = sizeof(alive_cells) / sizeof(struct _live);
-
-void draw()
-{
-  for(int row = 0; row < rows; ++row) {
-    for(int col = 0; col < cols; ++col) {
-      if(grid[row][col] == ALIVE) {
-        u8g.drawBox(left + col * 4, top + row * 3, 3, 2);
-      }
-    }
-  }
-  
-}
 
 void setup()
 {
@@ -58,15 +48,31 @@ void setup()
   struct _live* cur = alive_cells;
   
   for(i = 0; i < rows; ++i) {
-    for(j = 0; j < cols; ++j) {
-      grid[i][j] = DEAD;
+    for(j = 0; j < bytes; ++j) {
+      set_state(i, j, 0);
     }
   }
 
   for(i = 0; i < num_alive_cells; ++i) {
-    grid[cur->y][cur->x] = ALIVE;
+    set_state(cur->y, cur->x, 1);
     ++cur;
   }  
+
+  u8g.setFont(u8g_font_u8glib_4);
+}
+
+void draw()
+{
+  for(int row = 0; row < rows; ++row) {
+    for(int col = 0; col < cols; ++col) {
+      if(state(row, col)) {
+        u8g.drawBox(left + col * 3, top + row * 3, 2, 2);
+      }
+    }
+  }
+
+  u8g.setPrintPos(116, 63);
+  u8g.print(generation);
 }
 
 void loop()
@@ -80,27 +86,27 @@ void loop()
   while(u8g.nextPage());
 
   update_next();
+  ++generation;
   
-  delay(500);
+  delay(330);
 }
 
 void update_next(void) {
-  boolean next[rows][cols];
-  int     i, j, n;
+  int i, j, n;
   
   for(i = 0; i < rows; ++i) {
     for(j = 0; j < cols; ++j) {
       n = neighbours(i, j);
 
-      if(grid[i][j] == ALIVE)
-        next[i][j] = (n == 2 || n == 3);  // Two or three neighbours sustains
+      if(state(i, j))
+        set_new_state(i, j, n == 2 || n == 3);  // Two or three neighbours sustains
       else
-        next[i][j] = (n == 3);            // Three neighbours is generative
+        set_new_state(i, j, n == 3);  // Three neighbours is generative
     }
   }  
 
   for(i = 0; i < rows; ++i) {
-    for(j = 0; j < cols; ++j) {
+    for(j = 0; j < bytes; ++j) {
       grid[i][j] = next[i][j];
     }
   }
@@ -109,9 +115,9 @@ void update_next(void) {
 int neighbours(int row, int col) {
   int n = 0;
   
-  for(int i = row - 1; i <= row + 1; ++i) {
-    for(int j = col - 1; j <= col + 1; ++j) {
-      if(state(i, j) == ALIVE)
+  for(int r = row - 1; r <= row + 1; ++r) {
+    for(int c = col - 1; c <= col + 1; ++c) {
+      if((c != col || r != row) && state(r, c))
         ++n;
     }
   }
@@ -121,9 +127,31 @@ int neighbours(int row, int col) {
 
 boolean state(int row, int col) {
   if(row < 0 || col < 0 || row >= rows || col >= cols)
-    return DEAD;
+    return 0;
 
-  return grid[row][col];
+  int cbyte = col >> 3;
+  int cmask = col & 0x7;
+
+  return grid[row][cbyte] & (1 << cmask);
 }
 
+void set_state(int row, int col, boolean state) {
+  int cbyte = col >> 3;
+  int cmask = col & 0x7;
+
+  if(state)
+    grid[row][cbyte] |= (1 << cmask);
+   else
+    grid[row][cbyte] &= ~(1 << cmask);   
+}
+
+void set_new_state(int row, int col, boolean state) {
+  int cbyte = col >> 3;
+  int cmask = col & 0x7;
+
+  if(state)
+    next[row][cbyte] |= (1 << cmask);
+   else
+    next[row][cbyte] &= ~(1 << cmask);   
+}
 
