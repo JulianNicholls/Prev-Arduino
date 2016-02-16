7e566f8f775fbf2c166974b323f863e47aa2b3a7
Julian Nicholls <juliannicholls29@gmail.com>
2016-02-16 11:29:49 +0000

    Change to 2x2 blocks with no margin
    Gives a 64x32 grid.

--- conway-life-128x64/conway-life-128x64.ino
+++ conway-life-128x64/conway-life-128x64.ino
@@ -2,10 +2,10 @@
 
 U8GLIB_ST7920_128X64_1X u8g(10);    // Hardware SPI, 10 = CS/CE, 11 = MOSI, 13 = SCK
 
-const int cols  = 128 / 3;  // 42
-const int rows  = 64 / 3;   // 21
+const int cols  = 128 / 2;  // 2 -> 64, 3 -> 42
+const int rows  = 64 / 2;   // 2 -> 32, 3 -> 21
 
-const int bytes = (cols >> 3) + 1;
+const int bytes = (cols >> 3); // + 1 only needed for /3
 
 const int top   = 0;
 const int left  = 0;
@@ -14,24 +14,30 @@ byte  grid[rows][bytes];
 byte  next[rows][bytes];
 int   generation = 0;
 
+// blinker-puffer.life
 struct _live {
   int y;
   int x;
+} alive_cells[] = {
+  {1, 15}, {1, 16}, {1, 17}, {1, 18},
+  {2, 15}, {2, 19},
+  {3, 15},
+  {4, 6}, {4, 7}, {4, 16}, {4, 19},
+  {5, 5}, {5, 6}, {5, 8}, {5, 9},
+  {6, 6}, {6, 7}, {6, 8}, {6, 9}, {6, 13},
+  {7, 7}, {7, 8}, {7, 12}, {7, 14}, {7, 15}, {7, 24}, {7, 29}, {7, 30}, {7, 31},
+  {8, 11}, {8, 15}, {8, 24}, {8, 29}, {8, 31},
+  {9, 7}, {9, 8}, {9, 12}, {9, 14}, {9, 15}, {9, 24}, {9, 29}, {9, 30}, {9, 31},
+  {10, 6}, {10, 7}, {10, 8}, {10, 9}, {10, 13},
+  {11, 5}, {11, 6}, {11, 8}, {11, 9},
+  {12, 6}, {12, 7}, {12, 16}, {12, 19},
+  {13, 15},
+  {14, 15}, {14, 19},
+  {15, 15}, {15, 16}, {15, 17}, {15, 18},
 };
 
-struct _live alive_cells[] = {
-  {1, 25},
-  {2, 23}, {2, 25},
-  {3, 13}, {3, 14}, {3, 21}, {3, 22}, {3, 35}, {3, 36},
-  {4, 12}, {4, 16}, {4, 21}, {4, 22}, {4, 35}, {4, 36},
-  {5, 1}, {5, 2}, {5, 11}, {5, 17}, {5, 21}, {5, 22},
-  {6, 1}, {6, 2}, {6, 11}, {6, 15}, {6, 17}, {6, 18}, {6, 23}, {6, 25},
-  {7, 11}, {7, 17}, {7, 25},
-  {8, 12}, {8, 16},
-  {9, 13}, {9, 14},
-};
 
-const int num_alive_cells = sizeof(alive_cells) / sizeof(struct _live);
+const int num_alive_cells = sizeof(alive_cells) / sizeof(alive_cells[0]);
 
 void setup()
 {
@@ -57,7 +63,7 @@ void draw()
   for(int r = 0; r < rows; ++r) {
     for(int c = 0; c < cols; ++c) {
       if(state(r, c)) {
-        u8g.drawBox(left + c * 3, top + r * 3, 2, 2);
+        u8g.drawBox(left + c * 2, top + r * 2, 2, 2);
       }
     }
   }
@@ -79,7 +85,7 @@ void loop()
   update_next();
   ++generation;
   
-  delay(50);
+//  delay(330);
 }
 
 void update_next(void) {
@@ -89,10 +95,8 @@ void update_next(void) {
     for(j = 0; j < cols; ++j) {
       n = neighbours(i, j);
 
-      if(state(i, j))
-        set_new_state(i, j, n == 2 || n == 3);  // Two or three neighbours sustains
-      else
-        set_new_state(i, j, n == 3);  // Three neighbours is generative
+      // Two or three neighbours sustains, Three neighbours is generative
+      set_new_state(i, j, (state(i, j) && n == 2) || n == 3);  
     }
   }  
 
