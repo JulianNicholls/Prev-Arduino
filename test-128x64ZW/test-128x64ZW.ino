44f13f0e6bcfa3afc87f0b7ff8ad960c7958d31a
Julian Nicholls <juliannicholls29@gmail.com>
2016-03-09 19:30:34 +0000

    Add stepper-driver.
    Uses 128x64 LCD over SPI for display.
    The 4x4 Keypad and stepper motor are attached to MCP23017.
    Update 128x64 basic display test.

--- test-128x64ZW/test-128x64ZW.ino
+++ test-128x64ZW/test-128x64ZW.ino
@@ -1,11 +1,36 @@
-
 #include "U8glib.h"
 
 U8GLIB_ST7920_128X64_1X u8g(10);    // Hardware SPI, 10 = CS/CE, 11 = MOSI, 13 = SCK
 
+void setup(void)
+{ 
+}
+
+void loop(void)
+{
+  static bool done = false;
+  
+  // Picture loop
+  
+  if(!done) {
+    u8g.firstPage();
+  
+    do {
+      draw();
+    } 
+    while(u8g.nextPage());
+
+    done = true;
+  }
+  
+  // Don't Rebuild the picture after some delay
+  
+  delay(200);
+}
+
 void draw(void) {
   // graphic commands to redraw the complete screen should be placed here
-  //u8g.setFont(u8g_font_04b_03b);
+  // u8g.setFont(u8g_font_04b_03b);
 
   u8g.setFont(u8g_font_helvB10);
   u8g.drawStr(5, 17, "Testing HW SPI");
@@ -23,27 +48,12 @@ void draw(void) {
   u8g.drawStr(70, 36, "courB08 6");
 
   u8g.setFont(u8g_font_5x8);
-  u8g.drawStr(5, 45, "This will be readable");
-  u8g.drawStr(5, 52, "and there is room for 3");
-  u8g.drawStr(5, 59, "three lines of 24.");
+  u8g.drawStr(5, 45, "2 lines of 24 characters");
+  u8g.drawStr(5, 52, "XXXXXXXXXXXXXXXXXXXXXXXX");
+  u8g.drawStr(0, 59, "12345678901234567890123456");
 
   u8g.drawFrame(0, 0, 128, 64);
   u8g.drawFrame(2, 2, 124, 60);
 }
 
-void setup(void)
-{
 
-}
-
-void loop(void)
-{
-  // picture loop
-  u8g.firstPage();
-  do {
-    draw();
-  } while(u8g.nextPage());
-
-  // rebuild the picture after some delay
-  delay(200);
-}
